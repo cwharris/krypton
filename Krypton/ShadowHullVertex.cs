956bc807ed6708cdec7be1e69c92d3bb4230699d
Christopher Harris <xixonia@gmail.com>
2017-02-03 19:22:21 -0600

    Cleanup: ShadowHullVertex (#8)
    * .
    
    * .
    
    * .
    
    * .

--- Krypton/ShadowHullVertex.cs
+++ Krypton/ShadowHullVertex.cs
@@ -5,51 +5,40 @@ namespace Krypton
 {
     public struct ShadowHullVertex : IVertexType
     {
-        /// <summary>
-        /// The position of the vertex
-        /// </summary>
         public Vector2 Position;
 
-        /// <summary>
-        /// The normal of the vertex
-        /// </summary>
         public Vector2 Normal;
-
-        /// <summary>
-        /// The color of vertex
-        /// </summary>
+        
         public Color Color;
-
-        private static readonly VertexDeclaration mVertexDeclaration;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public VertexDeclaration VertexDeclaration { get { return ShadowHullVertex.mVertexDeclaration; } }
-
-        static ShadowHullVertex()
+        
+        public VertexDeclaration VertexDeclaration => Declaration;
+        
+        public ShadowHullVertex(
+            Vector2 position,
+            Vector2 normal,
+            Color color)
         {
-            VertexElement[] elements = new VertexElement[]
-            {
-                new VertexElement(0, VertexElementFormat.Vector2, VertexElementUsage.Position, 0),
-                new VertexElement(8, VertexElementFormat.Vector2, VertexElementUsage.Normal,0),
-                new VertexElement(16, VertexElementFormat.Color, VertexElementUsage.Color,0),
-            };
-
-            mVertexDeclaration = new VertexDeclaration(elements);
+            Position = position;
+            Normal = normal;
+            Color = color;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="position"></param>
-        /// <param name="normal"></param>
-        /// <param name="opacity"></param>
-        public ShadowHullVertex(Vector2 position, Vector2 normal, Color color)
-        {
-            this.Position = position;
-            this.Normal = normal;
-            this.Color = color;
-        }
+        private static readonly VertexDeclaration Declaration =
+            new VertexDeclaration(
+                new VertexElement(
+                    offset: 0,
+                    elementFormat: VertexElementFormat.Vector2,
+                    elementUsage: VertexElementUsage.Position,
+                    usageIndex: 0),
+                new VertexElement(
+                    offset: 8,
+                    elementFormat: VertexElementFormat.Vector2,
+                    elementUsage: VertexElementUsage.Normal,
+                    usageIndex: 0),
+                new VertexElement(
+                    offset: 16,
+                    elementFormat: VertexElementFormat.Color,
+                    elementUsage: VertexElementUsage.Color,
+                    usageIndex: 0));
     }
 }
